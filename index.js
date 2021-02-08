094e1c7987a25f4d1adb624fe2467a4facaa6ba8
Leo <l3omtz@gmail.com>
2021-02-08 15:35:14 -0500

    update to use node packages for grcp

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const grpc = require('grpc');
-// Use grpc load method
-const notesProto = grpc.load('notes.proto');
-// Instantiate grpc server
-const server = new grpc.Server();
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const PROTO_PATH = './notes.proto';
 
-// Bind it our localhost with port 50051 and pass server credential insecure 
-server.bind('127.0.0.1:50051',
-grpc.ServerCredentials.createInsecure());
+// Load notes proto with options
+const packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+const notes_proto = grpc.loadPackageDefinition(packageDefinition).notes;
 
-console.log('Server running at http://127.0.0.1:50051');
-
-// Invoke the start method of the server to start out gRPC server.  
-server.start();
+const main = () => {
+    // Instantiate grpc server
+    const server = new grpc.Server();
+    // Bind it our localhost with port 50051 and pass server credential insecure 
+    server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+        console.log('gRPC is serving....');
+        // Invoke the start method of the server to start out gRPC server.  
+        server.start();
+    });
+}
+main();
