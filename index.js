f73628ea4bf9f4b80aa0483554b9a1832bdf02ed
Leo <l3omtz@gmail.com>
2021-02-09 11:35:06 -0500

    add list service

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 const PROTO_PATH = './notes.proto';
+const notesDB = './db.js';
 
 // Load notes proto with options
 const packageDefinition = protoLoader.loadSync(
@@ -14,9 +15,26 @@ const packageDefinition = protoLoader.loadSync(
     });
 const notes_proto = grpc.loadPackageDefinition(packageDefinition).notes;
 
+/**
+ * 
+ * @param {*} call 
+ * Request from client
+ * @param {*} callback
+ * function we will invoke to return the response to the client
+ */
+const list = (call, callback) => {
+    callback(null, notesDB);
+  }
+
 const main = () => {
     // Instantiate grpc server
     const server = new grpc.Server();
+
+    // Invoke addService method and pass NoteService service as first param
+    // and function handler that will be invoked when the client calls the List method as the second param
+    server.addService(notes_proto.NotesService.service, { list });
+
+
     // Bind it our localhost with port 50051 and pass server credential insecure 
     server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
         console.log('gRPC is serving....');
@@ -24,4 +42,5 @@ const main = () => {
         server.start();
     });
 }
+
 main();
